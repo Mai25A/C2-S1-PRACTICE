5a77d95500d0df1a0ef7436e7f5b90ab6002b4a9
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-05-05 20:00:19 +0700

    first commit

---
+++ exercise-1/src/components/header/index.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import '../../index.css';
+function Header(){
+    return <>
+            <header className="block">
+                <h2 className='header'>Welcome to this course!</h2>
+            </header>
+        </>
+}
+export default Header;
